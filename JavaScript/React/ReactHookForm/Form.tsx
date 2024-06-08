c27c55f317119088a356eb0378d83350f6f54295
Blacky Wolf <16546603+BlackyWolf@users.noreply.github.com>
2024-06-08 15:40:24 -0500

    Create Form.tsx

---
+++ JavaScript/React/ReactHookForm/Form.tsx
@@ -0,0 +1,41 @@
+import { joinCss } from '@/utilities';
+import { Children, PropsWithChildren, createElement } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Properties = PropsWithChildren<{
+    className?: string;
+    defaultValues?: never;
+    onSubmit: SubmitHandler<FieldValues>;
+}>;
+
+export const Form = ({
+    children,
+    className,
+    defaultValues,
+    onSubmit
+}: Properties) => {
+    const { handleSubmit, register } = useForm({ defaultValues });
+
+    const css = joinCss('space-y-6', className);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={css}>
+            {Children.map(children, child => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const element: any = child;
+
+                if (element?.props?.name) {
+                    return createElement(element.type, {
+                        ...{
+                            ...element.props,
+                            register,
+                            key: element.props.name
+                        }
+                    });
+                }
+
+                return child;
+            })}
+        </form>
+    );
+};
